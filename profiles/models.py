42ba07f0c44e2b7a0bb4a0c0294708e12faa19ef
SamarZiadat <samarziadat@gmail.com>
2023-10-01 11:30:42 +0000

    build: profiles app

---
+++ profiles/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+
+class Profile(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=255, blank=True)
+    location = models.CharField(max_length=255, blank=True)
+    portfolio_url = models.URLField(max_length=255, blank=True)
+    job_title = models.CharField(max_length=255, blank=True)
+    current_employer = models.CharField(max_length=255, blank=True)
+    about = models.TextField(blank=True)
+    work_experience = models.TextField(blank=True)
+    education = models.TextField(blank=True)
+    certifications = models.TextField(blank=True)
+    profile_picture = models.ImageField(
+        upload_to="images/", default="../default_profile_xbjblo"
+    )
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"{self.owner}'s profile"
+
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(owner=instance)
+
+
+post_save.connect(create_profile, sender=User)
