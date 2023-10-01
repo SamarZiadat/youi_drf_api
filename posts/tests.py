a58029022ff96d3878fc7d99583f9539cd04a984
SamarZiadat <samarziadat@gmail.com>
2023-10-01 13:10:22 +0000

    build: automated testing for posts

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,43 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Post
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class PostListViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username="samar", password="pass")
+
+    def test_can_list_posts(self):
+        samar = User.objects.get(username="samar")
+        Post.objects.create(owner=samar, content="test content")
+        response = self.client.get("/posts/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        print(response.data)
+        print(len(response.data))
+
+    def test_user_not_logged_in_cant_create_post(self):
+        response = self.client.post("/posts/", {"content": "test content"})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class PostDetailViewTests(APITestCase):
+    def setUp(self):
+        samar = User.objects.create_user(username="samar", password="pass")
+        brian = User.objects.create_user(username="brian", password="pass")
+        Post.objects.create(owner=samar, content="test content")
+        Post.objects.create(owner=brian, content="test content")
+
+    def test_can_retrieve_post_using_valid_id(self):
+        response = self.client.get("/posts/1/")
+        self.assertEqual(response.data["content"], "test content")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_cant_retrieve_post_using_invalid_id(self):
+        response = self.client.get("/posts/999/")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_user_cant_update_another_users_post(self):
+        self.client.login(username="samar", password="pass")
+        response = self.client.put("/posts/2/", {"content": "new test content"})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
