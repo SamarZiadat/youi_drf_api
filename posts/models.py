a7ca56f823f8aa1b3b91f5b0efd29689691d1028
SamarZiadat <samarziadat@gmail.com>
2023-10-01 12:42:11 +0000

    build: posts app, including model and serializers

---
+++ posts/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
+
+
+class Post(models.Model):
+    """
+    Post model, related to 'owner', i.e. a User instance.
+    Default image set so that we can always reference image.url.
+    """
+
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    content = models.TextField(blank=True)
+    tags = TaggableManager(blank=True)
+    image = models.ImageField(
+        upload_to="images/", default="../default_post_e675n6", blank=True
+    )
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"{self.id}"
--- youi_drf_api/settings.py
+++ youi_drf_api/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     "cloudinary",
     "rest_framework",
     "django_filters",
+    "taggit",
     "profiles",
 ]
 
